5b576cf340dbbb5935f06df53c5c7f91de285088
Shuvam Banerji Seal <sbs22ms076@iiserkol.ac.in>
2025-12-14 14:44:04 +0530

    Fix base path for Organization Page deployment

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <BrowserRouter basename="/Synapse-iiserk.github.io">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </HelmetProvider>
